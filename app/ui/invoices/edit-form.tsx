93f0ae956b6d3dcb536fc6caeadee990097379bc
waling04 <126455318+waling04@users.noreply.github.com>
2024-02-09 17:54:33 +0700

    second commit

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,13 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
+import { useFormState } from 'react-dom';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoice Edit Page',
+};
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,8 +24,12 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState = {message: null, errors: {}};
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
   return (
-    <form>
+    <form action={dispatch}>
+        <input type="hidden" name="id" value={invoice.id} />
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
