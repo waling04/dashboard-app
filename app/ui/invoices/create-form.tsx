e7951f39d44242eb6f13cea49357bc87c039ba5b
waling04 <126455318+waling04@users.noreply.github.com>
2024-02-10 19:37:33 +0700

    edit typo

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -120,8 +120,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </div>
           </div>
           <div id="status-error" aria-live="polite" aria-atomic="true">
-                {state.errors?.status &&
-                  state.errors.status.map((error: string) => (
+                {state.error?.status &&
+                  state.error.status.map((error: string) => (
                     <p className="mt-2 text-sm text-red-500" key={error}>
               {error}
             </p>
