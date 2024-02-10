76bdc5714588754fa5e6be394752243bf19b8c94
waling04 <126455318+waling04@users.noreply.github.com>
2024-02-10 18:59:40 +0700

    third commit

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -23,7 +23,7 @@ export default async function Page({
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
 
-  const totalPages = await fetchInvoicesPages(query);
+  const totalPages = await fetchInvoicesPages('query');
 
   return (
     <div className="w-full">
